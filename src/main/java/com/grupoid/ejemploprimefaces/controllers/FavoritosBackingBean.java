315134f41fec9c6a79b403ebdcc240e72f291f15
José Antonio Parejo Maestre <japarejo@gmail.com>
2024-03-05 13:56:23 +0100

    Fixed minor errors!

--- src/main/java/com/grupoid/ejemploprimefaces/controllers/FavoritosBackingBean.java
+++ src/main/java/com/grupoid/ejemploprimefaces/controllers/FavoritosBackingBean.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
